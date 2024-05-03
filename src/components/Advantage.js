1961ddab486747482383184170c0c040dda375fd
Tiavina Michael Ralainirina <ralainirinatiavina@gmail.com>
2024-05-03 15:22:45 +0300

    change icons positions

--- src/components/Advantage.js
+++ src/components/Advantage.js
@@ -4,8 +4,9 @@ const useStyles = createUseStyles((theme) => ({
   textContainer: {
     borderRadius: 164.706,
     background: theme.color.green[560],
-    minWidth: 95.53,
-    padding: 19.765
+    // minWidth: 95.53,
+    // padding: 19.765
+    padding: "16px 24px"
   },
   text: {
     color: theme.color.green[710],
@@ -19,34 +20,61 @@ const useStyles = createUseStyles((theme) => ({
   },
   content: {
     position: "relative",
-    background: "red",
-    padding: "16px 30px"
+    // background: "red",
+    padding: "24px 34px"
   },
   icons: {
     position: "absolute"
   },
   semiCircleRightIcon: {
     composes: "$icons",
-    left: 0,
+    left: 16,
+    top: 2
+  },
+  point01Icon: {
+    composes: "$icons",
+    left: 45,
+    top: 9
+  },
+  point02Icon: {
+    composes: "$icons",
+    right: 44,
     top: 0
   },
-  icon02: {
+  point03Icon: {
     composes: "$icons",
-    left: 20
-  },
-  icon03: {
-    composes: "$icons",
-    right: 20
-  },
-  icon04: {
-    composes: "$icons",
-    right: 5,
-    top: 40
+    right: 0,
+    top: 36
   },
   circleIcon: {
     composes: "$icons",
-    right: 5,
-    top: 40
+    right: 0,
+    bottom: 38
+  },
+  point04Icon: {
+    composes: "$icons",
+    right: 18,
+    bottom: 10
+  },
+  point04Icon: {
+    composes: "$icons",
+    right: 18,
+    bottom: 10
+  },
+  semiCircleLeftIcon: {
+    composes: "$icons",
+    right: 0,
+    bottom: 0
+  },
+  lineIcon: {
+    composes: "$icons",
+    left: 40,
+    bottom: 3
+  },
+  starIcon: {
+    composes: "$icons",
+    left: 0,
+    bottom: 16
   }
 }));
 
@@ -66,22 +94,42 @@ const Advantage = () => {
         <img
           alt=""
           src="/images/fidelity-point-01.svg"
-          className={classes.icon02}
+          className={classes.point01Icon}
         />
         <img
           alt=""
           src="/images/fidelity-point-01.svg"
-          className={classes.icon03}
+          className={classes.point02Icon}
         />
         <img
           alt=""
           src="/images/fidelity-point-01.svg"
-          className={classes.icon04}
+          className={classes.point03Icon}
         />
         <img
           alt=""
           src="/images/fidelity-circle.svg"
           className={classes.circleIcon}
+        />
+        <img
+          alt=""
+          src="/images/fidelity-point-01.svg"
+          className={classes.point04Icon}
+        />
+        <img
+          alt=""
+          src="/images/fidelity-semi-circle-left.svg"
+          className={classes.semiCircleLeftIcon}
+        />
+        <img
+          alt=""
+          src="/images/fidelity-line.svg"
+          className={classes.lineIcon}
+        />
+        <img
+          alt=""
+          src="/images/fidelity-star.svg"
+          className={classes.starIcon}
         />
         {/* <img
           alt=""
