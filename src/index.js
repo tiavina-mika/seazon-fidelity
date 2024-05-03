344372bf5bb76de392e0c423215317fdf2c905bc
Tiavina Michael Ralainirina <ralainirinatiavina@gmail.com>
2024-05-03 10:56:37 +0300

    rename file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Demo from './demo';
+import App from './App';
 
-ReactDOM.render(<Demo />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
     
