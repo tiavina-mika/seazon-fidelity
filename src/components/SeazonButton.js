d4fe77fc0adc4ae3190d32856fde4ddc9636a066
Tiavina Michael Ralainirina <ralainirinatiavina@gmail.com>
2024-05-03 11:41:59 +0300

    fix import

--- src/components/SeazonButton.js
+++ src/components/SeazonButton.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { createUseStyles } from "react-jss";
 import classNames from "classnames";
-import { isFoodcheriService, toOneText } from "./utils/utils";
+import { isFoodcheriService, toOneText } from "../utils/utils";
 
 const useStyles = createUseStyles((theme) => ({
   container: {
